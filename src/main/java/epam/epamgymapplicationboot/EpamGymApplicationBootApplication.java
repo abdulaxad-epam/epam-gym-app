5ddd3d03dff18793d0099d3af846177fb9108db0
Abdulaxad <abdulaxad_isroilov@epam.com>
2025-04-03 17:33:50 +0500

    initial commit

---
+++ src/main/java/epam/epamgymapplicationboot/EpamGymApplicationBootApplication.java
@@ -0,0 +1,13 @@
+package epam.epamgymapplicationboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpamGymApplicationBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpamGymApplicationBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/epam/epamgymapplicationboot/EpamGymApplicationBootApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.epamgymapplicationboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpamGymApplicationBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
